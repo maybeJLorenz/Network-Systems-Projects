42ad1f1dcfd6a6e2b00233400fc87f3fb03766d0
maybeJLorenz <167827887+maybeJLorenz@users.noreply.github.com>
2024-10-30 00:04:32 -0700

    implementation added

--- lab5/lab5controller.py
+++ lab5/lab5controller.py
@@ -4,6 +4,7 @@
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
+from pox.lib.address import IPAddr ## added code
 
 log = core.getLogger()
 
@@ -13,62 +14,89 @@ class Firewall (object):
   A Connection object for that switch is passed to the __init__ function.
   """
   def __init__ (self, connection):
-    # Keep track of the connection to the switch so that we can
-    # send it messages!
+    
     self.connection = connection
-
-    # This binds our PacketIn event listener
     connection.addListeners(self)
 
-  def do_firewall (self, packet, packet_in):
-    # The code in here will be executed for every packet
-
+    ## implemented stuff to accept()
     def accept():
-      # Write code for an accept function
-      #important to include:
-      #msg.actions.append(of.ofp_action_output(port=of.OFPP_NORMAL))
+      msg = of.ofp_flow_mod()
+      msg.match = of.ofp_match.from_packet(packet)
+      msg.idle_timeout = 45
+      msg.hard_timeout = 600
+      msg.actions.append(of.ofp_action_output(port=of.OFPP_NORMAL))
+      self.connection.send(msg)
       print("Packet Accepted - Flow Table Installed on Switches")
 
+    ## added to drop()
     def drop():
-      # Write code for a drop function
+      msg = of.ofp_flow_mod()
+      msg.match = of.ofp_match.from_packet(packet)
+      msg.idle_timeout = 45
+      msg.hard_timeout = 600
+      self.connection.send(msg)
       print("Packet Dropped - Flow Table Installed on Switches")
 
     # Write firewall code 
-    print("Example Code")
+    def do_firewall (self, packet, packet_in):
+      ip_header = packet.find('ipv4')
+      tcp_header = packet.find('tcp')
+      udp_header = packet.find('udp')
 
-    # Hints:
-    #
-    # To check the source and destination of an IP packet, you can use
-    # the header information... For example:
-    #
-    # ip_header = packet.find('ipv4')
-    #
-    # if ip_header.srcip == "1.1.1.1":
-    #   print "Packet is from 1.1.1.1"
-    #
-    # Important Note: the "is" comparison DOES NOT work for IP address
-    # comparisons in this way. You must use ==.
-    #
-    # To drop packets, simply omit the action .
+      # Define IPs
+      laptop_ip = IPAddr("10.1.1.2")
+      server_ip = IPAddr("10.1.1.1")
+      lights_ip = IPAddr("10.1.2.1")
+      fridge_ip = IPAddr("10.1.2.2")
 
-  def _handle_PacketIn (self, event):
-    """
-    Handles packet in messages from the switch.
-    """
+      if ip_header is None:
+        # Rule #1: allow ARP and ICMP for general connectivity
+        if packet.find('arp') or packet.find('icmp'):
+            self.accept(packet, packet_in)
+        else:
+            self.drop(packet, packet_in)  # Drop other non-IP packets
+        return
 
-    packet = event.parsed # This is the parsed packet data.
-    if not packet.parsed:
-      log.warning("Ignoring incomplete packet")
-      return
+        # Rule #2: Web Traffic - allow all TCP traffic between laptop and server
+        if tcp_header:
+            if (ip_header.srcip == laptop_ip and ip_header.dstip == server_ip) or \
+               (ip_header.srcip == server_ip and ip_header.dstip == laptop_ip):
+                self.accept(packet, packet_in)
+                return
 
-    packet_in = event.ofp # The actual ofp_packet_in message.
-    self.do_firewall(packet, packet_in)
+        # Rule #3a: IoT Access - allow all TCP traffic between laptop and lights
+        if tcp_header and ip_header.srcip == laptop_ip and ip_header.dstip == lights_ip:
+            self.accept(packet, packet_in)
+            return
 
-def launch ():
-  """
-  Starts the components
-  """
-  def start_switch (event):
-    log.debug("Controlling %s" % (event.connection,))
-    Firewall(event.connection)
-  core.openflow.addListenerByName("ConnectionUp", start_switch)
+        # Rule #3b: IoT Acess - allow all UDP traffic between laptop and fridge
+        if udp_header and ip_header.srcip == laptop_ip and ip_header.dstip == fridge_ip:
+            self.accept(packet, packet_in)
+            return
+
+        # Rule #4: Laptop/Server General Management - allow all UDP traffic between laptop and server
+        if udp_header:
+            if (ip_header.srcip == laptop_ip and ip_header.dstip == server_ip) or \
+               (ip_header.srcip == server_ip and ip_header.dstip == laptop_ip):
+                 self.accept(packet, packet_in)
+                 return
+
+        # Default Deny: Drop all other traffic
+        self.drop(packet, packet_in)
+
+    
+    def _handle_PacketIn (self, event):
+      packet = event.parsed # This is the parsed packet data.
+      if not packet.parsed:
+        log.warning("Ignoring incomplete packet")
+        return
+  
+      packet_in = event.ofp # The actual ofp_packet_in message.
+      self.do_firewall(packet, packet_in)
+
+  def launch ():
+    
+    def start_switch (event):
+      log.debug("Controlling %s" % (event.connection,))
+      Firewall(event.connection)
+    core.openflow.addListenerByName("ConnectionUp", start_switch)
