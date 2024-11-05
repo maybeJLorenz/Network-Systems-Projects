1e787905dd1ee06794dfee766aaf8959352475eb
maybeJLorenz <167827887+maybeJLorenz@users.noreply.github.com>
2024-11-04 16:36:29 -0800

    added IPs and firewall descriptions

--- lab6/lab6_controller_skel.py
+++ lab6/lab6_controller_skel.py
@@ -1,4 +1,7 @@
 # Lab5 Skeleton
+#
+#     Last Modified: november 4, 4:30pm
+# 
 
 from pox.core import core
 
@@ -21,11 +24,68 @@ class Routing (object):
     # switch_id - the switch which received this packet
 
     # Your code here
-      ip_header = packet.find('ipv4')
-      tcp_header = packet.find('tcp')
-      udp_header = packet.find('udp')
-      icmp_header = packet.find('icmp')
-      arp_header = packet.find('arp')  
+    ip_header = packet.find('ipv4')
+    tcp_header = packet.find('tcp')
+    udp_header = packet.find('udp')
+    icmp_header = packet.find('icmp')
+
+    ## Defining IPs
+      # Univeristy Data Center
+    exam_ip = "10.100.100.2/24"
+    web_ip = "10.100.100.20"
+    dns_ip = "10.100.100.56"
+
+      # IT Department LAN
+    itWS_ip = "10.40.3.30"
+    itPC_ip = "10.40.3.254"
+
+      # Faculty LAN
+    facultyWS_ip = "10.0.1.2/24"
+    printer_ip = "10.0.1.4/24"
+    facultyPC_ip = "10.0.1.4/24"
+
+      # Student Housing LAN
+    student1_ip = "10.0.2.2/24"
+    student2_ip = "10.0.2.40"
+    lab_ip = "10.0.2.40"
+
+      # Internet
+    trusted_ip = "10.0.203.6/32"
+    guest1_ip = "10.0.198.6/32"
+    guest2_ip = "10.0.198.10/32"
+
+    def accept():
+      msg = of.ofp_flow_mod()
+      msg.data = packet_in
+      msg.match = of.ofp_match.from_packet(packet)
+      msg.idle_timeout = 45
+      msg.hard_timeout = 600
+      msg.actions.append(of.ofp_action_output(port=of.OFPP_NORMAL))
+      msg.buffer_id = packet_in.buffer_id
+      self.connection.send(msg)
+      print("Packet Accepted - Flow Table Installed on Switches")
+    
+    def drop():
+      msg = of.ofp_flow_mod()
+      msg.match = of.ofp_match.from_packet(packet)
+      msg.idle_timeout = 45
+      msg.hard_timeout = 600
+      print("Packet Dropped - Flow Table Installed on Switches")
+
+    # Rule #1: icmp between Student Housing, Faculty, and IT Dep. 
+    #    NO: Univeristy or Internet
+
+    # Rule #2: tcp between University, IT Dep, Faculty, Student Housing, and trustedPC
+    #       - NO: Internet
+    #       - Only Faculty LAN may access exam server
+
+
+    # Rule #3: udp between University, IT Dep, Faculty, and Student Housing
+    #       - NO: Internet
+
+
+    # Rule #4: all other traffic is dropped
+    drop()
       
     pass
 
