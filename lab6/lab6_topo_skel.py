b0c2b3ad8370eef7fffbd9fc4b73388c7ea87ffa
maybeJLorenz <167827887+maybeJLorenz@users.noreply.github.com>
2024-11-06 17:43:41 -0800

    update

--- lab6/lab6_topo_skel.py
+++ lab6/lab6_topo_skel.py
@@ -16,21 +16,21 @@ class MyTopology(Topo):
       ## Workstations
     facultyWS = self.addHost('facultyWS', ip='10.0.1.2/24', mac="00:00:02:00:01", defaultRoute="facultyWS-eth1")
     labWS = self.addHost('labWS', ip='10.0.2.3/24', mac="00:00:03:00:01", defaultRoute="labWS-eth1")
-    itWS = self.addHost('itWS', ip='10.40.3.30', mac="00:00:04:00:01", defaultRoute="itWS-eth1")
+    itWS = self.addHost('itWS', ip='10.40.3.30/24', mac="00:00:04:00:01", defaultRoute="itWS-eth1")
     
       ## Laptops
     facultyPC = self.addHost('facultyPC', ip='10.0.1.4/24', mac="00:00:02:00:02", defaultRoute="facultyPC-eth1")
     studentPC1 = self.addHost('studentPC1', ip='10.0.2.2/24', mac="00:00:03:00:02", defaultRoute="studentPC1-eth1")
-    studentPC2 = self.addHost('studentPC2', ip='10.0.2.40', mac="00:00:03:00:03", defaultRoute="studentPC2-eth1")
-    itPC = self.addHost('itPC', ip='10.40.3.254', mac="00:00:04:00:02", defaultRoute="itPC-eth4")
+    studentPC2 = self.addHost('studentPC2', ip='10.0.2.40/24', mac="00:00:03:00:03", defaultRoute="studentPC2-eth1")
+    itPC = self.addHost('itPC', ip='10.40.3.254/24', mac="00:00:04:00:02", defaultRoute="itPC-eth1")
     trustedPC = self.addHost('trustedPC', ip='10.0.203.6/32', mac="00:00:01:00:01", defaultRoute="trustedPC-eth1")
     guest1 = self.addHost('guest1', ip='10.0.198.6/32', mac="00:00:01:00:02", defaultRoute="guest1-eth1")
     guest2 = self.addHost('guest2', ip='10.0.198.10/32', mac="00:00:01:00:03", defaultRoute="guest2-eth1")
     
       ## Servers
     examServer = self.addHost('examServer', ip='10.100.100.2/24', mac="00:00:05:00:01", defaultRoute="examServer-eth1")
-    webServer = self.addHost('webServer', ip='10.100.100.20', mac="00:00:05:00:02", defaultRoute="webServer-eth1")
-    dnsServer = self.addHost('dnsServer', ip='10.100.100.56', mac="00:00:05:00:03", defaultRoute="dnsServer-eth1")
+    webServer = self.addHost('webServer', ip='10.100.100.20/24', mac="00:00:05:00:02", defaultRoute="webServer-eth1")
+    dnsServer = self.addHost('dnsServer', ip='10.100.100.56/24', mac="00:00:05:00:03", defaultRoute="dnsServer-eth1")
     
       ## Other
     printer = self.addHost('printer', ip='10.0.1.3/24', mac="00:00:02:00:03", defaultRoute="printer-eth1")
@@ -44,36 +44,36 @@ class MyTopology(Topo):
 
     ## Adding Links
       ## University Data Center
-    self.addLink(examServer, s5, port1=1, port2=2)
-    self.addLink(webServer, s5, port1=1, port2=3)
-    self.addLink(dnsServer, s5, port1=1, port2=4)
-    self.addLink(s5, s1, port1=5, port2=5)
+    self.addLink(examServer, s5, port1=1, port2=1)
+    self.addLink(webServer, s5, port1=1, port2=2)
+    self.addLink(dnsServer, s5, port1=1, port2=3)
+    self.addLink(s5, s1, port1=5, port2=5) ## switch-switch
 
       ## IT Department LAN
-    self.addLink(itWS, s4, port1=1, port2=2)
-    self.addLink(itPC, s4, port1=1, port2=3)
-    self.addLink(s4, s1, port1=4, port2=4)
+    self.addLink(itWS, s4, port1=1, port2=1)
+    self.addLink(itPC, s4, port1=1, port2=2)
+    self.addLink(s4, s1, port1=4, port2=4) ## swtich-switch
 
       ## Student Housing LAN
-    self.addLink(studentPC1, s3, port1=1, port2=2)
-    self.addLink(studentPC2, s3, port1=1, port2=3)
+    self.addLink(studentPC1, s3, port1=1, port2=1)
+    self.addLink(studentPC2, s3, port1=1, port2=2)
     self.addLink(labWS, s3, port1=1, port2=4)
-    self.addLink(s3, s1, port1=3, port2=3)
+    self.addLink(s3, s1, port1=3, port2=3) ## switch-switch
 
       ## Faculty LAN
-    self.addLink(facultyWS, s2, port1=1, port2=2)
+    self.addLink(facultyWS, s2, port1=1, port2=1)
     self.addLink(printer, s2, port1=1, port2=3)
     self.addLink(facultyPC, s2, port1=1, port2=4)
-    self.addLink(s2, s1, port1=2, port2=2)
+    self.addLink(s2, s1, port1=2, port2=2) ## switch-switch
 
       ## Internet
-    self.addLink(trustedPC, s1, port1=1, port2=2)
-    self.addLink(guest1, s1, port1=1, port2=3)
-    self.addLink(guest2, s1, port1=1, port2=4)
+    self.addLink(trustedPC, s1, port1=1, port2=1)
+    self.addLink(guest1, s1, port1=1, port2=6)
+    self.addLink(guest2, s1, port1=1, port2=7)
+
+      ## Links between Switches
    
     # laptop1 = self.addHost('Laptop1', ip='200.20.2.8/24',defaultRoute="Laptop1-eth1")
-
-    # switch1 = self.addSwitch('s1')
 
     # self.addLink(laptop1, switch1, port1=1, port2=2)
     
@@ -87,4 +87,3 @@ if __name__ == '__main__':
   net.start() #Starts mininet
   CLI(net) #Opens a command line to run commands on the simulated topology
   net.stop() #Stops mininet
-
