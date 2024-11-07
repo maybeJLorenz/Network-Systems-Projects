5f8689513239f6f88530b8d11a0f5ec14fc0616b
maybeJLorenz <167827887+maybeJLorenz@users.noreply.github.com>
2024-11-07 13:25:47 -0800

    layout for code somewhat done

--- lab6/lab6_controller_skel.py
+++ lab6/lab6_controller_skel.py
@@ -1,12 +1,11 @@
-
 # Lab5 Skeleton
 #
-#     Last Modified: november 6, 9:45pm
+#     Last Modified: november 7, 1:25pm
 # 
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from netaddr import *
+import ipaddress
 
 log = core.getLogger()
 
@@ -31,7 +30,7 @@ class Routing (object):
       msg = of.ofp_flow_mod()
       msg.data = packet_in
       msg.match = of.ofp_match.from_packet(packet)
-      msg.actions.append(of.ofp_action_output(port=1)) ## all ending ports (hosts) are 1
+      msg.actions.append(of.ofp_action_output(port=end_port))
       msg.buffer_id = packet_in.buffer_id
       self.connection.send(msg)
       print("Packet Accepted - Flow Table Installed on Switches")
@@ -52,34 +51,87 @@ class Routing (object):
     it_subnet = "10.40.3.0/24"
     university_subnet = "10.100.100.0/24"
 
-        
-    trustedPC_ip = IPAddress("10.0.203.6/32") 
-    exam_server_ip = IPAddress("10.100.100.2/24")
+    facultyWS_ip = ipaddress.IPv4Address("10.0.1.2")
+    printer_ip = ipaddress.IPv4Address("10.0.1.3")
+    facultyPC_ip = ipaddress.IPv4Address("10.0.1.4")
 
+    studentPC1_ip = ipaddress.IPv4Address("10.0.2.2")
+    studentPC2_ip = ipaddress.IPv4Address("10.0.2.3")
+    labWS_ip = ipaddress.IPv4Address("10.0.2.40")
+
+    itWS_ip = ipaddress.IPv4Address("10.40.3.30")
+    itPC_ip = ipaddress.IPv4Address("10.40.3.254")
+
+    trustedPC_ip = ipaddress.IPv4Address("10.0.203.6") 
+    exam_server_ip = ipaddress.IPv4Address("10.100.100.2")
+
+    
 
     # port_on_switch - the port on which this packet was received
     # switch_id - the switch which received this packet
 
     # Rule #1: icmp between Student Housing, Faculty, and IT Dep. 
     #    NO: Univeristy or Internet subnet 
-    allowed_r1 = [faculty_subnet, student_subnet, it_subnet]
 
-    if icmp_header and ip_header:
-        src_ip = IPAddress(ip_header.srcip)
-        dst_ip = IPAddress(ip_header.dstip)
-        
-        for subnet in allowed_r1:
-            if src_ip in subnet and dst_ip in subnet:
-                accept()
-                return
+    if ip_header:
+      src_ip = ip_header.srcip
+      dst_ip = ip_header.dstip
+
+    if switch_id == 1 and icmp_header:
+      if dst_ip in faculty_subnet:
+         accept(2)
+         return
+      if dst_ip in student_subnet:
+         accept(3)
+         return
+      if dst_ip in it_subnet:
+         accept(4)
+         return
+
+    if switch_id == 2 and icmp_header: # inside faculty switch (s2)
+      if dst_ip == facultyWS_ip:
+        accept(1)
+        return
+      elif dst_ip == printer_ip:
+        accept(3)
+        return
+      elif dst_ip == facultyPC_ip:
+        accept(4)
+        return
+      else:
+         accept(2)
+         return
+      
+    
+    if switch_id == 3 and icmp_header: # inside student switch (s3)
+      if dst_ip == studentPC1_ip:
+        accept(1)
+        return
+      
+      if dst_ip == labWS_ip:
+        accept(2)
+        return
+      
+      if dst_ip == studentPC2_ip:
+        accept(4)
+        return
+
+    if switch_id == 4 and icmp_header: # inside IT switch (s4)
+      if dst_ip == itWS_ip:
+        accept(1)
+        return
+      
+      if dst_ip == itPC_ip:
+        accept(2)
+        return
       
         
     # Rule #2: tcp between University, IT Dep, Faculty, Student Housing, and trustedPC
     #       - NO: Internet subnet
     #       - Only Faculty LAN may access exam server
     if tcp_header and ip_header:
-        src_ip = IPAddress(ip_header.srcip)
-        dst_ip = IPAddress(ip_header.dstip)
+        src_ip = ipaddress.IPv4Address(ip_header.srcip)
+        dst_ip = ipaddress.IPv4Address(ip_header.dstip)
 
         allowed_r2 = [university_subnet, it_subnet, faculty_subnet, student_subnet]
         for subnet in allowed_r2:
@@ -100,8 +152,8 @@ class Routing (object):
     # # Rule #3: udp between University, IT Dep, Faculty, and Student Housing
     # #       - NO: Internet subnet
     if udp_header and ip_header:
-        src_ip = IPAddress(ip_header.srcip)
-        dst_ip = IPAddress(ip_header.dstip)
+        src_ip = ipaddress.IPv4Address(ip_header.srcip)
+        dst_ip = ipaddress.IPv4Address(ip_header.dstip)
 
         allowed_r3 = [university_subnet, it_subnet, faculty_subnet, student_subnet]
         for subnet in allowed_r3:
