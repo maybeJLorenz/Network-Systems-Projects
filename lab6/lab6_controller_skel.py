8d0f30ce6d8959e670dbd33b903300410e9cf5a2
maybeJLorenz <167827887+maybeJLorenz@users.noreply.github.com>
2024-11-07 00:41:29 -0800

    edited rules (go ask for help)

--- lab6/lab6_controller_skel.py
+++ lab6/lab6_controller_skel.py
@@ -25,17 +25,13 @@ class Routing (object):
     # switch_id - the switch which received this packet
 
     # Your code here
-      ## Primary variables for packet
-    ip_header = packet.find('ipv4')
-    tcp_header = packet.find('tcp')
-    udp_header = packet.find('udp')
-    icmp_header = packet.find('icmp')
+      
 
     def accept(end_port):
       msg = of.ofp_flow_mod()
       msg.data = packet_in
       msg.match = of.ofp_match.from_packet(packet)
-      msg.actions.append(of.ofp_action_output(port=end_port))
+      msg.actions.append(of.ofp_action_output(port=1)) ## all ending ports (hosts) are 1
       msg.buffer_id = packet_in.buffer_id
       self.connection.send(msg)
       print("Packet Accepted - Flow Table Installed on Switches")
@@ -44,13 +40,21 @@ class Routing (object):
       msg = of.ofp_flow_mod()
       msg.match = of.ofp_match.from_packet(packet)
       print("Packet Dropped - Flow Table Installed on Switches")
-
     
+    ## Primary variables for packets
+    ip_header = packet.find('ipv4')
+    tcp_header = packet.find('tcp')
+    udp_header = packet.find('udp')
+    icmp_header = packet.find('icmp')
     
     faculty_subnet = "10.0.1.0/24"
     student_subnet = "10.0.2.0/24"
     it_subnet = "10.40.3.0/24"
     university_subnet = "10.100.100.0/24"
+
+        
+    trustedPC_ip = IPAddress("10.0.203.6/32") 
+    exam_server_ip = IPAddress("10.100.100.2/24")
 
 
     # port_on_switch - the port on which this packet was received
@@ -58,46 +62,52 @@ class Routing (object):
 
     # Rule #1: icmp between Student Housing, Faculty, and IT Dep. 
     #    NO: Univeristy or Internet subnet 
-    if icmp_header:
-      subnet = IPNetwork(faculty_subnet)
-      if IPAddress(ip_header.srcip) in subnet and IPAddress(ip_header.dstip) in subnet:
-        accept()
-        return
-      subnet = IPNetwork(student_subnet)
-      if IPAddress(ip_header.srcip) in subnet and IPAddress(ip_header.dstip) in subnet:
-        accept()
-      subnet = IPNetwork(it_subnet)
-      if IPAddress(ip_header.srcip) in subnet and IPAddress(ip_header.dstip) in subnet:
-        accept()
-      
-      # if port_on_switch == 5:
-      #   if switch_id == 5: ## faculty subnet
-      #     if ip_header.dstip == "10.100.100.0/24":
-      #       end_port = 3
-      #       accept(end_port)
-      #       return
-        
-      
+    allowed_r1 = [faculty_subnet, student_subnet, it_subnet]
 
+    if icmp_header and ip_header:
+        src_ip = IPAddress(ip_header.srcip)
+        dst_ip = IPAddress(ip_header.dstip)
+        
+        for subnet in allowed_r1:
+            if src_ip in subnet and dst_ip in subnet:
+                accept()
+                return
+      
+        
     # Rule #2: tcp between University, IT Dep, Faculty, Student Housing, and trustedPC
     #       - NO: Internet subnet
     #       - Only Faculty LAN may access exam server
-    # if tcp_header:
-    #   if (switch_id != 5) or (ip_header.srcip == trustedPC_ip or ip_header.dstip == trustedPC_ip):
-    #     ## drop tcp traffic if a non-Faculty LAN is trying to access Exam 
-    #     if (ip_header.dstip == exam_ip or ip_header.srcip == exam_ip) and port_on_switch != 's2-eth0':
-    #       drop()
-    #       return
-    #     else:
-    #       accept()
-    #       return
+    if tcp_header and ip_header:
+        src_ip = IPAddress(ip_header.srcip)
+        dst_ip = IPAddress(ip_header.dstip)
+
+        allowed_r2 = [university_subnet, it_subnet, faculty_subnet, student_subnet]
+        for subnet in allowed_r2:
+            if src_ip in subnet and dst_ip in subnet:
+                accept()
+                return
+                
+        # faculty LAN has access  exam server
+        if src_ip in faculty_subnet and dst_ip == exam_server_ip:
+            accept()
+            return
+
+        if src_ip == trustedPC_ip or dst_ip == trustedPC_ip:
+            if any([src_ip in subnet or dst_ip in subnet for subnet in allowed_r2]):
+                accept()
+                return
                 
     # # Rule #3: udp between University, IT Dep, Faculty, and Student Housing
     # #       - NO: Internet subnet
-    # if udp_header:
-    #   if switch_id != 5:
-    #     accept()
-    #     return
+    if udp_header and ip_header:
+        src_ip = IPAddress(ip_header.srcip)
+        dst_ip = IPAddress(ip_header.dstip)
+
+        allowed_r3 = [university_subnet, it_subnet, faculty_subnet, student_subnet]
+        for subnet in allowed_r3:
+            if src_ip in subnet and dst_ip in subnet:
+                accept()
+                return
 
     # Rule #4: all other traffic is dropped
     drop()
