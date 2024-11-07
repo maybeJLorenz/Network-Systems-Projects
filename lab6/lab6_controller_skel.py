a4932e8b20f2961ab34947d8138b2b06b011d628
maybeJLorenz <167827887+maybeJLorenz@users.noreply.github.com>
2024-11-06 21:44:55 -0800

    Update lab6_controller_skel.py

--- lab6/lab6_controller_skel.py
+++ lab6/lab6_controller_skel.py
@@ -1,11 +1,12 @@
+
 # Lab5 Skeleton
 #
-#     Last Modified: november 5, 11:30pm
+#     Last Modified: november 6, 9:45pm
 # 
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-from netaddr import IPAddress, IPNetwork
+from netaddr import *
 
 log = core.getLogger()
 
@@ -29,38 +30,12 @@ class Routing (object):
     tcp_header = packet.find('tcp')
     udp_header = packet.find('udp')
     icmp_header = packet.find('icmp')
-    
 
-    ## Defining IPs
-      # Univeristy Data Center
-    exam_ip = "10.100.100.2/24"
-    web_ip = "10.100.100.20/24"
-    dns_ip = "10.100.100.56/24"
-
-      # IT Department LAN
-    itWS_ip = "10.40.3.30/24"
-    itPC_ip = "10.40.3.254/24"
-
-      # Faculty LAN
-    facultyWS_ip = "10.0.1.2/24"
-    printer_ip = "10.0.1.3/24"
-    facultyPC_ip = "10.0.1.4/24"
-
-      # Student Housing LAN
-    student1_ip = "10.0.2.2/24"
-    student2_ip = "10.0.2.40/24"
-    lab_ip = "10.0.2.3/24" 
-
-      # Internet
-    trustedPC_ip = "10.0.203.6/32"
-    guest1_ip = "10.0.198.6/32"
-    guest2_ip = "10.0.198.10/32"
-
-    def accept():
+    def accept(end_port):
       msg = of.ofp_flow_mod()
       msg.data = packet_in
       msg.match = of.ofp_match.from_packet(packet)
-      msg.actions.append(of.ofp_action_output(port=1))
+      msg.actions.append(of.ofp_action_output(port=end_port))
       msg.buffer_id = packet_in.buffer_id
       self.connection.send(msg)
       print("Packet Accepted - Flow Table Installed on Switches")
@@ -75,34 +50,32 @@ class Routing (object):
     faculty_subnet = "10.0.1.0/24"
     student_subnet = "10.0.2.0/24"
     it_subnet = "10.40.3.0/24"
-    ##university_subnet = "10.100.100.0/24"
-
-    def isInSubnet(givenIP, givenSubnet):
-      ip = IPAddress(givenIP)
-      subnet = IPNetwork(givenSubnet)
-      return ip in subnet
+    university_subnet = "10.100.100.0/24"
 
 
     # port_on_switch - the port on which this packet was received
     # switch_id - the switch which received this packet
 
     # Rule #1: icmp between Student Housing, Faculty, and IT Dep. 
-    #    NO: Univeristy or Internet subnet
-    src_ip = ip_header.srcip
-    dst_ip = ip_header.dstip
-
-    allowed_subnets = [
-      faculty_subnet, student_subnet, it_subnet
-    ]
-
+    #    NO: Univeristy or Internet subnet 
     if icmp_header:
-
+      subnet = IPNetwork(faculty_subnet)
+      if IPAddress(ip_header.srcip) in subnet and IPAddress(ip_header.dstip) in subnet:
+        accept()
+        return
+      subnet = IPNetwork(student_subnet)
+      if IPAddress(ip_header.srcip) in subnet and IPAddress(ip_header.dstip) in subnet:
+        accept()
+      subnet = IPNetwork(it_subnet)
+      if IPAddress(ip_header.srcip) in subnet and IPAddress(ip_header.dstip) in subnet:
+        accept()
       
-      if any(isInSubnet(src_ip, subnet) and isInSubnet(dst_ip, subnet)):
-        for subnet in allowed_subnets:
-          accept()
-      else:
-        drop()
+      # if port_on_switch == 5:
+      #   if switch_id == 5: ## faculty subnet
+      #     if ip_header.dstip == "10.100.100.0/24":
+      #       end_port = 3
+      #       accept(end_port)
+      #       return
         
       
 
@@ -151,5 +124,3 @@ def launch ():
     log.debug("Controlling %s" % (event.connection,))
     Routing(event.connection)
   core.openflow.addListenerByName("ConnectionUp", start_switch)
-
-
